58c90a9326ebd2c25862bc7b52a99991d7a8a1e8
Evgeny Novikov <evgeny.novikov@cedargame.com>
2021-05-08 12:59:52 +0300

    Глобальные правки в перемещаемый объект, вынес всякие свойсва в MovableObjectData

--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -2,16 +2,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Singleton;
+
+[System.Serializable]
+public class MovableObjectData
+{
+    [SerializeField] private SurfaceType objectType;
+    [SerializeField] private float movingSpeed;
+
+    [Header("Physics")]
+    [SerializeField] private Rigidbody rigidbodySelf;
+    [SerializeField] private Collider selfCollider;
+
+    public void SetMovable(MovableObject movableObject)
+    {
+        MovableObject = movableObject;
+    }
+
+    public MovableObject MovableObject { get; private set; }
+
+    public float MovingSpeed => movingSpeed;
+    public Rigidbody Rigidbody => rigidbodySelf;
+    public SurfaceType ObjectSurfaceType => objectType;
+    public Collider SelfCollider => selfCollider;
+    public GameObject GameObject => MovableObject.gameObject;
+}
 
 public class MovableObject : MonoBehaviour
 {
-    private MovementType movementType;
+    [SerializeField] MovableObjectData objectData;
 
-    public Action MovementTypeChanged; 
+    public event Action MovementTypeChanged;
+    public event Action Collisioned;
+
+    public MovementType MovementType { get; private set; }
+
+    private void Start()
+    {
+        objectData.SetMovable(this);
+    }
+
+    private void OnMouseDown()
+    {
+        MovementType?.Init(objectData);
+    }
+
+    private void OnMouseDrag()
+    {
+        MovementType?.Move();
+    }
+
+    private void OnMouseUp()
+    {
+        MovementType?.EndMove();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collisioned?.Invoke();
+    }
 
     public void SetMovementType(MovementType type)
     {
-        movementType = type;
+        if(type == null)
+        {
+            Debug.LogError($"MovementType on {name} is null!");
+            return;
+        }
+
+        MovementType = type;
 
         MovementTypeChanged?.Invoke();
     }
