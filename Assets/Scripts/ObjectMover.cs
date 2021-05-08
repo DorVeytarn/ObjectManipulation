50499cd583f53a302710738e65e3d7e36f3456b9
Evgeny Novikov <evgeny.novikov@cedargame.com>
2021-05-08 12:59:01 +0300

    Выборщик типа перемещения

---
+++ Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,48 @@
+﻿using Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    private ObjectSelector objectSelector;
+    private UserInput userInput;
+    private MovableObject currentObject;
+
+    private void Start()
+    {
+        objectSelector = S.DI<ObjectSelector>();
+        userInput = S.DI<UserInput>();
+
+        objectSelector.ObjectSelected += OnObjectSelected;
+        userInput.MovementKeyActivated += OnMovementKeyActivated;
+    }
+
+    private void OnDestroy()
+    {
+        objectSelector.ObjectSelected -= OnObjectSelected;
+        userInput.MovementKeyActivated -= OnMovementKeyActivated;
+    }
+
+    private void OnMovementKeyActivated(UserInput.MovementType movementType)
+    {
+        MovementType newType = null;
+
+        switch (movementType)
+        {
+            case UserInput.MovementType.Free:
+                newType = new FreeMovement();
+                break;
+            case UserInput.MovementType.Sticking:
+                newType = new StickingMovement();
+                break;
+        }
+
+        currentObject.SetMovementType(newType);
+    }
+
+    private void OnObjectSelected(MovableObject movableObject)
+    {
+        currentObject = movableObject;
+    }
+}
