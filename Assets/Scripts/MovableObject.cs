cb29f6bda30544411312a68b68491df930bf8148
Evgeny Novikov <evgeny.novikov@cedargame.com>
2021-05-10 21:20:07 +0300

    вынес класс даты в отдельный файл

--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -1,38 +1,12 @@
 ﻿using System;
 using UnityEngine;
 
-[System.Serializable]
-public class MovableObjectData
-{
-    [SerializeField] private SurfaceType objectType;
-    [SerializeField] private float movingSpeed;
-    [SerializeField] private float rotatingSpeed;
-
-    [Header("Physics")]
-    [SerializeField] private Rigidbody rigidbodySelf;
-    [SerializeField] private Collider selfCollider;
-
-    public void SetMovable(MovableObject movableObject)
-    {
-        MovableObject = movableObject;
-    }
-
-    public MovableObject MovableObject { get; private set; }
-
-    public float MovingSpeed => movingSpeed;
-    public float RotatingSpeed => rotatingSpeed;
-    public Rigidbody Rigidbody => rigidbodySelf;
-    public SurfaceType ObjectSurfaceType => objectType;
-    public Collider SelfCollider => selfCollider;
-    public GameObject GameObject => MovableObject.gameObject;
-}
-
 public class MovableObject : MonoBehaviour
 {
     [SerializeField] MovableObjectData objectData;
 
     public event Action MovementTypeChanged;
-    public event Action Collisioned;
+    public event Action TargetSurfaceCollisioned;
 
     public MovementType MovementType { get; private set; }
 
@@ -58,7 +32,8 @@ public class MovableObject : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Collisioned?.Invoke();
+        if(collision.gameObject.TryGetComponent(out Surface surface) && surface.Type == objectData.ObjectSurfaceType)
+            TargetSurfaceCollisioned?.Invoke();
     }
 
     public void SetMovementType(MovementType type)
