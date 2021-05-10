89a2e4c577cfc7304894c8682e4e3fd479e0ab48
Evgeny Novikov <evgeny.novikov@cedargame.com>
2021-05-10 21:19:07 +0300

    красивость для подсветки выбранного объекта

---
+++ Assets/Scripts/SelectingEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Singleton;
+
+public class SelectingEffect : MonoBehaviour
+{
+    [SerializeField] private MovableObject movable;
+    [SerializeField] private Outline outline;
+
+    private ObjectSelector objectSelector;
+
+    private void Start()
+    {
+        objectSelector = S.DI<ObjectSelector>();
+
+        objectSelector.ObjectSelected += ChangeEffectVisibility;
+    }
+
+    private void OnDestroy()
+    {
+        objectSelector.ObjectSelected -= ChangeEffectVisibility;
+    }
+
+    private void ChangeEffectVisibility(MovableObject movable)
+    {
+        outline.enabled = this.movable == movable;
+    }
+}
